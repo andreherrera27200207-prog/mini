7830edd21a5bfd315936f1be40936a39160a27ef
Fabriccio <andreherrera27200207@gmail.com>
2026-01-21 19:00:52 +0100

    commit wip3

--- src/Libro.java
+++ src/Libro.java
@@ -4,5 +4,46 @@ public class Libro {
     private String autor;
     private String categoria;
 
-    
+    public Libro() {
+    }
+
+    public Libro(int id_libro, String nombre_Libro, String autor, String categoria) {
+        this.id_libro = id_libro;
+        this.nombre_Libro = nombre_Libro;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+
+    public int getId_libro() {
+        return id_libro;
+    }
+
+    public void setId_libro(int id_libro) {
+        this.id_libro = id_libro;
+    }
+
+    public String getNombre_Libro() {
+        return nombre_Libro;
+    }
+
+    public void setNombre_Libro(String nombre_Libro) {
+        this.nombre_Libro = nombre_Libro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
 }
