86616d924479aac4fd50772fc627e383a53e595c
Fabriccio <andreherrera27200207@gmail.com>
2026-01-21 19:36:01 +0100

    Gestor Libro  wip 1

--- src/GestorLibro.java
+++ src/GestorLibro.java
@@ -3,65 +3,69 @@ public class GestorLibro {
     private Libro[] auxilaLibros;
 
     private int aumento_secuencial;
+    private final int MAX = 100;
 
+    public GestorLibro() {
 
-    public GestorLibro(int capacidad) {
-
-        libro_array = new Libro[capacidad];
-        auxilaLibros = new Libro[capacidad];
+        libro_array = new Libro[MAX];
+        auxilaLibros = new Libro[MAX];
         aumento_secuencial = 0;
 
     }
 
-    public void agregar_Libros_nuevos(String nombreLibro, String autorLibro, String categoria) {
+    public boolean agregar_Libros_nuevos(Libro libro) {
 
-        Libro lib = new Libro();
-   
-        lib.setNombre_Libro(nombreLibro);
-        lib.setAutor(autorLibro);
-        lib.setCategoria(categoria);
-
-        libro_array[aumento_secuencial] = lib;
-        aumento_secuencial++;
-
-    }
-
-
-    public void eliminar_Libro(String nombre) {
-
-        for (int i = 0; i < aumento_secuencial; i++) {
-
-            if (libro_array[i].getNombre_Libro().equals(nombre)) {
-
-                libro_array[i].setNombre_Libro(null);
-                libro_array[i].setAutor(null);
-                libro_array[i].setCategoria(null);
-
-            }
+        if (aumento_secuencial < MAX) {
+            libro_array[aumento_secuencial] = libro;
+            aumento_secuencial++;
+            return true;
 
         }
+        return false;
 
     }
 
-    public String buscar_Libro(String nombreLibro, String autorLibro, String categoria) {
+    public int buscarIndicePorDNI(String dni){
+        for (int i = 0; i < aumento_secuencial; i++) {
+           if (libro_array[i].getId_libro().equals(dni)) {
+                return i;
+           }
+        }
+        return -1;
+    }
 
-        String retorno = "";
+
+
+    public boolean eliminar_Libro(String dni) {
+
+        int posicion = buscarIndicePorDNI(dni);
+
+        if (posicion!=-1){
+            for (int i = posicion+1; i < aumento_secuencial; i++) {
+                libro_array[i-1] = libro_array[i];
+            }
+            return true;
+        }
+        return false;
+
+    }
+
+    public Libro buscar_Libro(String nombreLibro, String autorLibro, String categoria) {
 
         for (int i = 0; i < aumento_secuencial; i++) {
 
             if (libro_array[i].getNombre_Libro().equals(nombreLibro) || libro_array[i].getAutor().equals(autorLibro)
                     || libro_array[i].getCategoria().equals(categoria)) {
 
-                retorno = libro_array[i].getNombre_Libro() + libro_array[i].getAutor() + libro_array[i].getCategoria();
+                return libro_array[i];
 
             }
 
         }
 
-        return retorno;
+        return null;
 
     }
-
 
     public String mostrar() {
 
@@ -73,7 +77,8 @@ public class GestorLibro {
                 todos_los_libros += "";
             } else {
 
-                todos_los_libros += " Nombre del Libro:  " + libro_array[i].getNombre_Libro() + "  Nombre del autor: " + libro_array[i].getAutor() + " Nombre de la categoría: "
+                todos_los_libros += " Nombre del Libro:  " + libro_array[i].getNombre_Libro() + "  Nombre del autor: "
+                        + libro_array[i].getAutor() + " Nombre de la categoría: "
                         + libro_array[i].getCategoria() + "\n";
             }
 
@@ -83,22 +88,14 @@ public class GestorLibro {
 
     }
 
-
     public Libro[] todos_los_libros() {
 
         return libro_array;
 
     }
 
-
-
     public int indice_incremento_servicioLibro() {
         return aumento_secuencial;
     }
-
-
-
-
-
 
 }
