07678fa10a0345710174e4041b95709a0bef1672
Pablo <piglesan@gmail.com>
2026-01-22 12:07:31 +0100

    GestorUsuarios_wip3

--- src/GestorUsuarios.java
+++ src/GestorUsuarios.java
@@ -12,4 +12,19 @@ public class GestorUsuarios {
         this.totalUsuarios=0;
     }
 
+    //Registrar usuario, solo puede hacerlo el admin
+    public boolean registrarUsuarios(String nombre, String contrasena, boolean admin){
+        //comprobar que hay espacio en el array
+        if (totalUsuarios >= MAX){
+            return false;
+        }
+
+        //registrar el nuevo usuario
+        int id = totalUsuarios+1;
+        Usuario nuevoUsuario = new Usuario(id, nombre, contrasena, admin);
+
+        usuarios[totalUsuarios]= nuevoUsuario;
+        totalUsuarios++;
+        return true;
+    }
 }
